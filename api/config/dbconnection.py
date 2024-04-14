49924829cfeaa55187196344650b496cf2cf6e45
Jense <jense.martinez@utp.edu.co>
2024-04-13 20:25:28 -0500

    DBconnection
    What was done?
    - create database connection.
    - create folders structure.
    - create FastApi instance.

---
+++ api/config/dbconnection.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a connection to the database
+# "mysql+pymysql://username:password@host:port/database"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:2332@localhost:3306/ecommerce"
+
+# database motor
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Session generator
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declarative base class
+Base = declarative_base()
