2b1b77016fe06312fa471becad18e97678abd7b7
Luisa Ramirez <luisa.ramirez3@utp.edu.co>
2024-04-14 23:20:18 -0500

    Create get product

--- api/routes/product.py
+++ api/routes/product.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter,Path
 from fastapi.responses import JSONResponse
-from typing import List
 from config.dbconnection import session
 from models.models import Products as ProductModel
 from fastapi.encoders import jsonable_encoder
