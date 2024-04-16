04be59cfbddba23bcddc3165eed3e102ccac7f21
SMejia115 <s.mejia6@utp.edu.co>
2024-04-16 13:21:56 -0500

    Individual Product View

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { MainHomeComponent } from './components/main-home/main-home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RangeValueAccessor } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { IndividualProductComponent } from './components/individual-product/individual-product.component';
 
 const routes: Routes = [
   { path: '', component: MainHomeComponent },
   { path: 'home/:page', component: MainHomeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'product/:id', component: IndividualProductComponent}
 ];
 
 @NgModule({
--- app/src/app/components/individual-product/individual-product.component.ts
+++ app/src/app/components/individual-product/individual-product.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-individual-product',
   templateUrl: './individual-product.component.html',
   styleUrl: './individual-product.component.css'
 })
-export class IndividualProductComponent {
+export class IndividualProductComponent implements OnInit{
+  images = [
+    {url: '../../../assets/img/products/Gorra1.png'},
+    {url: '../../../assets/img/products/Gorra2.png'},
+    {url: '../../../assets/img/products/Gorra1.png'},
+    {url: '../../../assets/img/products/Gorra2.png'}
+  ]
+  selectedImage: any;
+  previousImage: any;
+  quantity: number = 1;
+
+  ngOnInit(){
+    this.selectedImage = this.images[0];
+  }
+
+  selectImage(image: any, event: MouseEvent) {
+    if (event.type === 'click') {
+      // Si el evento es un clic, simplemente establece la imagen seleccionada
+      this.selectedImage = image;
+    } else if (event.type === 'mouseenter') {
+      // Si el evento es hover, guarda la imagen actual y establece la nueva imagen seleccionada
+      this.previousImage = this.selectedImage;
+      this.selectedImage = image;
+    } else if (event.type === 'mouseleave') {
+      // Si el evento es hover y el mouse deja la imagen, restaura la imagen principal anterior
+      this.selectedImage = this.previousImage;
+    }
+  }
+
+  setQuantity(quantity: number) {
+    if (this.quantity+quantity >= 1){
+      this.quantity = this.quantity+quantity;
+    }
+  }
 
 }
