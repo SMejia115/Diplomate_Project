d7639b068309ac9830eb8d7e81dcdabd606102ff
m4r14j0 <mariajose@esvyda.com>
2024-04-18 10:48:01 -0500

    created new components and added the image of the cart

---
+++ app/src/app/components/shop-product/shop-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-product',
+  templateUrl: './shop-product.component.html',
+  styleUrl: './shop-product.component.css'
+})
+export class ShopProductComponent {
+
+}
