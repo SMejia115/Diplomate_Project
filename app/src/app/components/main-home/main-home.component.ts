2ef226b4d3a32f5cea70e2c6239fc06d817b84a2
SMejia115 <s.mejia6@utp.edu.co>
2024-04-18 12:53:44 -0500

    Search bar

--- app/src/app/components/main-home/main-home.component.ts
+++ app/src/app/components/main-home/main-home.component.ts
@@ -12,6 +12,8 @@ export class MainHomeComponent implements OnInit {
   title!: string;
   imageRoute!: string;
   products: any;
+  allProducts: any;
+  searchTerm: string = ''
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
@@ -28,15 +30,40 @@ export class MainHomeComponent implements OnInit {
         this.imageRoute = '../../../assets/shop_home.jpg';
         this.http.get('http://localhost:8000/products').subscribe((data: any) => {
           console.log(data);  
+          this.allProducts = data;
           this.products = data;
         });
       } else if (this.page === 'caps') {
         this.title = 'Gorras';
         this.imageRoute = '../../../assets/img/backgrounds/caps_home.png';
+        this.http.get('http://localhost:8000/products/category/cap').subscribe((data: any) => {
+          console.log(data);  
+          this.allProducts = data;
+          this.products = data;
+        });
       } else if (this.page === 'watchs') {
         this.title = 'Relojes';
         this.imageRoute = '../../../assets/img/backgrounds/watchs_home.jpg';
+        this.http.get('http://localhost:8000/products/category/clock').subscribe((data: any) => {
+          console.log(data);  
+          this.allProducts = data;
+          this.products = data;
+        });
       }
     });
+  }
+
+  filterProducts(event: any): void {
+    this.searchTerm = event.target.value;
+    console.log(event.target.value)
+    if (!this.searchTerm) {
+      // Si el término de búsqueda está vacío, mostrar todos los productos
+      this.products = this.allProducts;
+    } else {
+      // Filtrar los productos por nombre
+      this.products = this.allProducts.filter((product: any) =>
+        product.productName.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 }
