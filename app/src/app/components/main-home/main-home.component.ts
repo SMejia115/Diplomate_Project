82ff61c44ec3b828e987654807c63d178033ec00
SMejia115 <s.mejia6@utp.edu.co>
2024-04-17 21:16:24 -0500

    Integration Commit

--- app/src/app/components/main-home/main-home.component.ts
+++ app/src/app/components/main-home/main-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-home',
@@ -10,17 +11,25 @@ export class MainHomeComponent implements OnInit {
   page!: string;
   title!: string;
   imageRoute!: string;
+  products: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
+    
+
     this.route.params.subscribe(params => {
       this.page = params['page']; // Aquí obtienes el valor del parámetro de ruta
       console.log(this.page);
+      
 
       if (this.page === 'shop') {
         this.title = 'Shop';
         this.imageRoute = '../../../assets/shop_home.jpg';
+        this.http.get('http://localhost:8000/products').subscribe((data: any) => {
+          console.log(data);  
+          this.products = data;
+        });
       } else if (this.page === 'caps') {
         this.title = 'Gorras';
         this.imageRoute = '../../../assets/img/backgrounds/caps_home.png';
