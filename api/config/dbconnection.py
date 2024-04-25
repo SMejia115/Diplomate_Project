c2e184d94015958f986190c6ef2cbbc79220f363
Jense <jense.martinez@utp.edu.co>
2024-04-24 19:41:17 -0500

    api-dbchange

--- api/config/dbconnection.py
+++ api/config/dbconnection.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # Create a connection to the database
 # "mysql+pymysql://username:password@host:port/database"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:2323@localhost:3306/railway"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:2332@localhost:3306/railway"
 # database motor
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
